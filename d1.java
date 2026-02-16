aa2c0927845bd41b375332ea397ecbb15bdb36a8
dinesh19-30 <dineshhariraman2004@gmail.com>
2026-02-16 21:02:02 +0530

    user input

--- d1.java
+++ d1.java
@@ -1,5 +1,13 @@
 public class d1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // program 1
+        int a = 5;
+        int b = 10; 
+        int c=20;
+        int sum = a + b + c;
+        System.out.println("The sum of a, b, and c is: " + sum);
+
     }
 }
---
+++ ui.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ui {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int intValue = sc.nextInt();
+
+        System.out.print("Enter a float value: ");
+        float floatValue = sc.nextFloat();
+
+        System.out.print("Enter a double value: ");
+        double doubleValue = sc.nextDouble();
+
+        System.out.print("Enter a character: ");
+        char charValue = sc.next().charAt(3);
+
+        System.out.print("Enter true or false: ");
+        boolean boolValue = sc.nextBoolean();
+
+        sc.nextLine();
+
+        System.out.print("Enter a string: ");
+        String stringValue = sc.nextLine();
+
+        System.out.println("\n--- User Entered Data ---");
+        System.out.println("Integer: " + intValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Character: " + charValue);
+        System.out.println("Boolean: " + boolValue);
+        System.out.println("String: " + stringValue);
+    }
+}
