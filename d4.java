f8fa0ff12748b1700a4adf37827a7e9c358814d9
dinesh19-30 <dineshhariraman2004@gmail.com>
2026-02-24 21:03:44 +0530

    condition

---
+++ d4.java
@@ -0,0 +1,24 @@
+public class d4 {
+    import java.util.Scanner;
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        if(a>b)
+        {
+            System.out.println("a is greater");
+            if(a%2==0)
+            {
+                System.out.println("this is even number");
+            }
+            else
+            {
+                System.out.println("this is odd number");
+            }    
+        }
+        else
+        {
+            System.out.println("b is greater");
+        }
+    }       
+}
